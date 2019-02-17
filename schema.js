a3e1e1c14522c016f4f97fb35cd5812331bc776b
edvinbasil <edvinbasil@gmail.com>
2019-02-17 15:06:41 +0530

    Fix customers query to work with json-server
    customers was trying to read the hardcoded data which wasnt available.
    Updated it to fetch from the json-server instead.

--- schema.js
+++ schema.js
@@ -54,7 +54,8 @@ const RootQuery = new GraphQLObjectType({
         customers: {
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args){
-                return customers
+                return axios.get('http://localhost:3000/customers/')
+                    .then(res => res.data)
             }
         }
 
